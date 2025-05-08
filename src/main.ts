9afc7f124bd2da95031e0873502659a8616da8e4
Andrei Robu <andreidrobu@gmail.com>
2025-05-08 13:22:25 +0300

    add dummy triangles animation

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,37 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const canvas = document.getElementById('canvas') as HTMLCanvasElement
+const ctx = canvas.getContext('2d')!
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+function drawTriangle() {
+  // just draw a random triangle
+  ctx.fillStyle = 'red'
+  ctx.beginPath()
+  ctx.moveTo(Math.random() * canvas.width, Math.random() * canvas.height)
+  ctx.lineTo(Math.random() * canvas.width, Math.random() * canvas.height)
+  ctx.lineTo(Math.random() * canvas.width, Math.random() * canvas.height)
+  ctx.closePath()
+  ctx.fill()
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function onFrame() {
+  ctx.fillStyle = 'rgba(255, 255, 255, 0.05)'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  for (let i = 0; i < 10; i++) {
+    drawTriangle()
+  }
+}
+
+function onFrameCallbackLoop() {
+  onFrame()
+  requestAnimationFrame(onFrameCallbackLoop)
+}
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  // onFrame() ?? not sure if this is needed
+}
+
+window.addEventListener('resize', resizeCanvas)
+resizeCanvas() // initialise canvas size
+
+onFrameCallbackLoop() // start the animation loop
